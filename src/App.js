5a5e8e37c60afc4cd8338fb162b18843a6bcf862
Abdul Halim <ahr.web.pro@gmail.com>
2022-01-04 19:01:51 +0600

    Update component hierarchy and update component style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import GlobalStyle from './styles/GlobalStyle';
+import { Container } from './styles/Main';
 import { reducer, initialState } from './reducer/noteReducer';
 import { NoteContextState, NoteContextDispatch } from './context/noteContext';
 import { lightTheme, darkTheme } from './styles/Theme';
@@ -17,10 +18,10 @@ function App() {
       <NoteContextDispatch.Provider value={dispatch}>
         <ThemeProvider theme={state.theme === 'dark' ? darkTheme : lightTheme}>
           <GlobalStyle />
-          <div className="container pb-5">
+          <Container className="container">
             <Header />
             <MainContent />
-          </div>
+          </Container>
         </ThemeProvider>
       </NoteContextDispatch.Provider>
     </NoteContextState.Provider>
--- src/styles/Main.js
+++ src/styles/Main.js
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  padding-bottom: 20px;
+`;
+
 export const Main = styled.main`
   margin-top: 2.5em;
 `;
